4937de7f84d8ec1004b7e6bc539355a6edff6219
alkindi17 <kindi1703@gmail.com>
2024-02-22 21:23:59 +0000

    skills update

--- components/sections/skills-section.js
+++ components/sections/skills-section.js
@@ -21,11 +21,11 @@ export default function SkillsSection() {
   // Render skills
   if (skillsData && !skillsError && !skillsIsLoading) {
     skills = (
-      <div className="mt-6 grid grid-cols-2 gap-4 max-sm:gap-3 md:grid-cols-3 lg:grid-cols-4">
-        {skillsData[activeSkill].map((skill) => {
+      <ul className="grid grid-cols-2 gap-4 max-sm:gap-3 md:grid-cols-3 lg:grid-cols-4">
+        {skillsData[activeSkill].map((skill, index) => {
           return (
-            <div
-              key={skill.name}
+            <li
+              key={index}
               className="card flex h-16 w-52 items-center gap-4 px-4 py-4 leading-4 max-md:h-14 max-md:gap-1 max-md:text-sm max-sm:h-12 max-sm:w-32 max-sm:pl-1 max-sm:pr-0 max-sm:text-xs"
             >
               <skill.icon
@@ -33,10 +33,10 @@ export default function SkillsSection() {
                 className="h-6 w-6 max-sm:h-4 max-sm:w-4"
               />
               {skill.name}
-            </div>
+            </li>
           );
         })}
-      </div>
+      </ul>
     );
   } else if (skillsError) {
     skills = "Error loading skills";
@@ -57,7 +57,9 @@ export default function SkillsSection() {
         setActiveTab={setActiveSkill}
       ></Tabs>
 
-      {skills}
+      <div className="mt-4 flex max-h-[65%] w-[100%] flex-col overflow-auto border-b border-t border-gray-300 py-4 dark:border-dark-accent-200 max-sm:max-h-[64%]">
+        <div className="flex justify-center">{skills}</div>
+      </div>
     </div>
   );
 }
